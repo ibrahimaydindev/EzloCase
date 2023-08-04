db89e3d8670c2b820f02ffa08fb3605fc36eff6d
ibrahimaydindev@gmail.com <aydinibrahim1692@gmail.com>
2023-08-04 23:24:23 +0300

    Layouts added

--- app/src/main/java/com/ibrahimaydindev/ezlocase/database/DeviceDao.kt
+++ app/src/main/java/com/ibrahimaydindev/ezlocase/database/DeviceDao.kt
@@ -1,18 +1,19 @@
 package com.ibrahimaydindev.ezlocase.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.ibrahimaydindev.ezlocase.model.Device
 
 interface DeviceDao {
-
-    @Query("SELECT * FROM news")
-    fun getAllArticles(): LiveData<List<News>>
+    @Query("SELECT * FROM devices")
+    fun getAllDevices(): LiveData<List<Device>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertArticle(article: News)
+    suspend fun insertDevice(device: Device)
 
     @Delete
-    suspend fun deleteArticle(article: News)
-    @Query("SELECT * FROM news WHERE id = :articleId")
-    suspend fun getArticle(articleId: Int): News
-
+    suspend fun deleteArticle(device: Device)
 }
